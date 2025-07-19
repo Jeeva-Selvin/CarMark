dadb7a28dc52f58180df7cf0bc7e46fd10795f87
Jeeva-Selvin <jeevaselvin2011@gmail.com>
2025-07-19 14:44:29 +0530

    flinal

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://carmark.netlify.app/login',
   credentials: true,
   sameSite:"none"
 }));
